2b81e12947e0d8a808dd35f8b01a271aa65c1cb0
IFLead <igos.321@gmail.com>
2017-03-11 10:29:38 +0300

    Initial commit

--- ITEnergy/settings.py
+++ ITEnergy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')r9$pijztp85zrd*j0m&b$j(a68*ssenw3xoz5j+an_(v&9m5)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iflead.pythonanywhere.com']
 
 # Application definition
 
